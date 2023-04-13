ecbde040d4aae91a56d1817aa3b866a2b998f689
Lev Maximov <lev.maximov@gmail.com>
2023-04-14 01:56:54 +0700

    readme, requirements, setup

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# numpy-illustrated
+
+[![pypi](https://img.shields.io/pypi/v/numpy-illustrated.svg)](https://pypi.python.org/pypi/numpy-illustrated)
+[![python](https://img.shields.io/pypi/pyversions/numpy-illustrated.svg)](https://pypi.org/project/numpy-illustrated/)
+![pytest](https://github.com/axil/numpy-illustrated/actions/workflows/python-package.yml/badge.svg)
+![Coverage Badge](img/coverage.svg)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![License](https://img.shields.io/pypi/l/numpy-illustrated)](https://pypi.org/project/numpy-illustrated/)
+
+This repo contains code for a number of helper functions mentioned in the [NumPy Illustrated](https://betterprogramming.pub/numpy-illustrated-the-visual-guide-to-numpy-3b1d4976de1d?sk=57b908a77aa44075a49293fa1631dd9b) guide.
+
+## Installation: 
+
+    pip install numpy-illustrated
+
+## Contents
+
+Basic operations:
+- `argmin(a)`
+- `argax(a)`
+
+## Testing
+
+Run `pytest` in the project root.
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='numpy-illustrated',
+    version='0.1',
+    author='Lev Maximov',
+    author_email='lev.maximov@gmail.com',
+    url='https://github.com/axil/numpy-illustrated',
+    description='Helper functions from the NumPy Illustrated guide',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.7",
+    install_requires=[
+        'pandas',
+        'bs4',              # for patch_series
+        'lxml',             # for bs4 
+        'IPython',          # for sidebyside
+    ],
+    packages=['pdi'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',    
+        'Programming Language :: Python :: 3.10',    
+        'Programming Language :: Python :: 3.11',    
+    ],
+    license='MIT License',
+    zip_safe=False,
+    keywords=['find', 'argmin', 'argmax', 'sort', 'irange', 'savez', 'numpy'],
+)
